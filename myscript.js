8658d3b3e6b19a885f9b2a9050d2ee00b3c1f7dd
LorenzoBernardini88 <lorenzo.bernardini88@gmail.com>
2021-12-20 22:15:42 +0100

    mod

--- myscript.js
+++ myscript.js
@@ -1,22 +1,23 @@
 
 let button = document.getElementById('button');//seleziono button richiamando id dell'elemento.
+//let imgHidden = document.getElementsByClassName('ms_game_img');
 
 button.addEventListener("click", function(){
-    
+    // imgHidden.classList.add('hidden');
     let scelta = document.getElementById('select').value;
-    let griglia = document.getElementById('ms_griglia');// selezioni div contenitore griglia richiamando la classe.
+    let griglia = document.getElementById('ms_griglia');// selezionio div contenitore griglia richiamando la classe.
     griglia.innerHTML = '';
     
     if(scelta == 1){
+        
         for(i=1; i<=100; i++){
             griglia.innerHTML += `<div class="casella">${i}</div>`;
         }
-
     }else if(scelta == 2) {
+        
         for(i=1; i<=81; i++){
             griglia.innerHTML += `<div class="casella">${i}</div>`;
         }
-    
     }else {
         
         for(i=1; i<=49; i++){
@@ -30,11 +31,8 @@ button.addEventListener("click", function(){
         box[i].addEventListener('click',function(){
 
             this.classList.add('color');
-
-        
         })
     }
-
 })
 
 
